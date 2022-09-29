0ba44572ff16e4af33c0eb55ba902db93ef66566
Artur Barbosa <89175768+fxbrartur@users.noreply.github.com>
2022-09-29 18:46:15 -0300

    rola

--- djangorest/settings.py
+++ djangorest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-5*^nm#s#$73won%+fd*s(4$)%@^l4&4@&r2g=5_amfmcsu_4f%"  # os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))  # True
@@ -146,4 +146,4 @@ REST_FRAMEWORK = {
     ],
 }
 
-INTERNAL_IPS = ["127.0.0.1"]
+INTERNAL_IPS = ["127.0.0.1",]
